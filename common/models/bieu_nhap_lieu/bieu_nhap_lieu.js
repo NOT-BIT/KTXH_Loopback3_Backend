9281f2a7addf9763beae668854f0b340660cb608
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2019-09-19 16:13:50 +0700

    Update json

---
+++ common/models/bieu_nhap_lieu/bieu_nhap_lieu.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports = function(BieuNhapLieu) {
+    const Promise = require('bluebird');
+
+    BieuNhapLieu.listBieuNhapLieu = async function(page, pageSize, queryData) {
+        
+    }
+}
